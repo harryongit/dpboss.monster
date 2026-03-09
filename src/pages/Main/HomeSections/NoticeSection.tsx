c4e2dceda0a4579b5bef0e921f939b7c7daf4550
narayani-org <connect@narayani.org>
2026-03-10 00:35:10 +0530

    w

--- src/pages/Main/HomeSections/NoticeSection.tsx
+++ src/pages/Main/HomeSections/NoticeSection.tsx
@@ -5,7 +5,7 @@ import { Phone, MessageCircle, Mail } from 'lucide-react';
 const NoticeSection = () => {
   // Phone & WhatsApp links with country code (+91 for India)
   const countryCode = "+91";
-  const phoneNumber = "8143175017";
+  const phoneNumber = "";
   const fullNumber = `${countryCode}${phoneNumber}`;
   const whatsappLink = `https://wa.me/${phoneNumber}`; // WhatsApp needs number without '+'
 
