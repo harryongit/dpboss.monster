af78135ce5857113af925fa79bb3512223e1c095
narayani-org <connect@narayani.org>
2026-06-23 12:59:43 +0530

    w

--- src/views/Main/HomeSections/HeaderLogo.tsx
+++ src/views/Main/HomeSections/HeaderLogo.tsx
@@ -6,7 +6,7 @@ const HeaderLogo = () => {
     <div className="top-0 z-50">
       <div className="p-1 flex justify-center">
 
-        <h1 className="border-[3px] border-red-500 rounded-[15px] px-6 py-3 bg-[#f3c08c] font-bold flex items-center justify-center gap-3 w-full max-w-[420px] m-0">
+        <h1 className="border-[3px] border-red-500 rounded-[15px] px-4 py-1 bg-[#f3c08c] font-bold flex items-center justify-center gap-3 w-full max-w-[420px] m-0">
 
           {/* Logo */}
           <img
@@ -14,7 +14,7 @@ const HeaderLogo = () => {
             alt="DP BOSS Fastest Satta Matka Result Logo"
             className="h-12 w-auto"
           />
-          <span className="text-2xl">BOSS.MONSTER</span>
+
 
 
         </h1>
